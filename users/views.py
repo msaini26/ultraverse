bd0b646172f1a17c74488eb7cc02877ed41642a0
cassjsdev <cassjes.m@gmail.com>
2022-01-28 14:54:14 -0800

    Created django framework, requirements.txt, and gitignore

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
