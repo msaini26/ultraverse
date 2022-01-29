730cf89d9ed0e64ba3352b18e7c127efec422562
cassjsdev <cassjes.m@gmail.com>
2022-01-28 20:50:11 -0800

    Modified login views.py and login.html

--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import *
 
 import logging
@@ -12,7 +14,24 @@ def register(request):
     return render(request, 'register.html')
 
 def login(request):
-    return render(request, 'login.html')
+    # if request.user.is_authenticated:
+    #     return redirect('home')
+    # else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                logger.info('Login successful. Redirecting to dashboard.')
+                return redirect('dashboard')
+            else: 
+                messages.info(request, 'username or password is incorrect')
+        
+        logger.info('Login template was rendered.')
+        return render(request, 'login.html')
 
 # This is temporary for testing on backend
 def developer(request):
