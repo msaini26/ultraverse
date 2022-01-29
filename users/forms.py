ef72a3b02c8ae76ef5603b29862ebf3f4737a68b
cassjsdev <cassjes.m@gmail.com>
2022-01-29 14:02:24 -0800

    edited forms.py

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms.widgets import Select
 
 from .models import Event, Comment
 
@@ -12,9 +13,14 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 class EventForm(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(EventForm, self).__init__(*args, **kwargs)
+        self.fields['author'].disabled = True
     class Meta:
         model = Event
         fields = ['author', 'title', 'description', 'link']
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
