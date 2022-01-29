a4c7b1030e8ac6c70a327db833f8c59c0c1facb4
cassjsdev <cassjes.m@gmail.com>
2022-01-28 17:06:05 -0800

    connected developer view to render template

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('developer/', views.developer, name='developer')
 ]
