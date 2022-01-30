d17a6822380539fd89ddba3947aa1875d7df9dd0
cassjsdev <cassjes.m@gmail.com>
2022-01-30 02:53:34 -0800

    azure deploy

--- ultraverse/settings.py
+++ ultraverse/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+# # Temporary addition relating to django_heroku
+# import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5v%qh)3&40b5^kfqi*^^bfp%^p#=rtzl=c+sb0#@qsnh^2&*#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # This is a LOGGING dict
@@ -175,3 +178,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# # Temporary addition relating to django_heroku dependency
+# django_heroku.settings(locals())
