138dbcc26b49b81943d759605043fd07477e0bb4
cassjsdev <cassjes.m@gmail.com>
2022-02-03 22:21:52 -0800

    modified gitignore and staticfiles location in settings

--- ultraverse/settings.py
+++ ultraverse/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticFiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
