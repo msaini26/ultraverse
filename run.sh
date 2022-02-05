2c1e55ad3a9cb2b25a4a909c3f7281022bb03379
zshawar <zshawar@uncc.edu>
2022-02-04 20:46:26 -0500

    Worked on fixing collect static

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Open browser port 8000
-open http://localhost:8000
-
-python manage.py runserver 8000
+python manage.py migrate
+python manage.py collectstatic
+python manage.py runserver 0.0.0.0:8000
